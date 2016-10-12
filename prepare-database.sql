59405ea5a0f58e245c6770f8f090ca75b31fac66
Juho Peltonen <juhopel@gmail.com>
2016-10-12 18:41:21 +0300

    fix sqoop script

---
+++ prepare-database.sql
@@ -0,0 +1,6 @@
+\connect sales_data;
+
+delete from browsing;
+delete from browsinghours;
+
+commit;
--- sqoop.sh
+++ sqoop.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+psql -f prepare-database.sql
+
 sqoop 'export' \
 --connect jdbc:postgresql://localhost/sales_data \
 --username hduser \
 --password hduser \
 --table browsing \
---update-key product_name \
 --export-dir 'out2/' \
 --driver org.postgresql.Driver \
 --connection-manager org.apache.sqoop.manager.GenericJdbcManager \
@@ -18,7 +19,6 @@ sqoop 'export' \
 --username hduser \
 --password hduser \
 --table browsinghours \
---update-key hour \
 --export-dir 'out4/' \
 --driver org.postgresql.Driver \
 --connection-manager org.apache.sqoop.manager.GenericJdbcManager \
